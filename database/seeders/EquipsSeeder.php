4f08bf3a94488867cc4b40c28fdb4d0508cf4c02
igomis <igomis@cipfpbatoi.es>
2025-09-03 19:51:53 +0200

    EquipsSeeder

--- database/seeders/EquipsSeeder.php
+++ database/seeders/EquipsSeeder.php
@@ -4,8 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\Equip;
 use App\Models\Estadi;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class EquipsSeeder extends Seeder
 {
@@ -30,5 +32,14 @@ class EquipsSeeder extends Seeder
             'titols' => 5,
         ]);
         Equip::factory()->count(10)->create();
+        foreach (Equip::all() as $equip){
+            User::create([
+                'name' => 'Manager  '.$equip->nom,
+                'email' => $equip->nom.'@manager.com',
+                'password' => Hash::make('1234'),
+                'role' => 'manager',
+                'equip_id' => $equip->id,
+            ]);
+        }
     }
 }

04504695b6e369655bda9714e30734c3c8f2d7b6
igomis <igomis@cipfpbatoi.es>
2025-09-07 09:14:45 +0200

    Traducció

--- resources/views/equips/create.blade.php
+++ resources/views/equips/create.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.equip')
-@section('title', 'Afegir nou equip')
+@section('title', __("Creació d'Equips"))
 
 @section('content')
-<h1 class="text-2xl font-bold mb-4">Afegir nou equip</h1>
+<h1 class="text-2xl font-bold mb-4"> __("Creació d'Equips")</h1>
 
 @if ($errors->any())
   <div class="bg-red-100 text-red-700 p-2 mb-4">
@@ -15,11 +15,11 @@
 <form action="{{ route('equips.store') }}" method="POST" enctype="multipart/form-data" class="space-y-4" >
   @csrf
   <div>
-    <label for="nom" class="block font-bold">Nom:</label>
+    <label for="nom" class="block font-bold">{{ __('Nom')}}:</label>
     <input type="text" name="nom" id="nom" value="{{ old('nom') }}" class="border p-2 w-full">
   </div>
   <div>
-        <label for="estadi_id" class="block font-bold">Estadi:</label>
+        <label for="estadi_id" class="block font-bold">{{__('Estadi')}}:</label>
         <select name="estadi_id" id="estadi_id" class="border p-2 w-full">
             @foreach ($estadis as $estadi)
                 <option value="{{ $estadi->id }}" {{ old('estadi_id') == $estadi->id ? 'selected' : '' }}>
@@ -28,15 +28,15 @@
             @endforeach
         </select>
         <div class="mb-4">
-          <label for="escut" class="block text-sm font-medium text-gray-700 mb-1">Escut:</label>
+          <label for="escut" class="block text-sm font-medium text-gray-700 mb-1">{{__('Escut')}}:</label>
           <input type="file" name="escut" id="escut"
                  class="w-full border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500">
         </div>
   </div>
   <div>
-    <label for="titols" class="block font-bold">Títols:</label>
+    <label for="titols" class="block font-bold">{{__('Títols')}}:</label>
     <input type="number" name="titols" id="titols" value="{{ old('titols') }}" class="border p-2 w-full">
   </div>
-  <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded">Afegir</button>
+  <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded">{{__('Crear Equip')}}</button>
 </form>
 @endsection
